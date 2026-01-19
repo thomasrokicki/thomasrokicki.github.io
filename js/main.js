dca2bd889a4829bc5bf9d102b14a9201da06fe0c
Thomas Rokicki <thomas.rokicki@centralesupelec.fr>
2026-01-19 18:04:10 +0100

    lala

--- js/main.js
+++ js/main.js
@@ -288,10 +288,6 @@
                       id: '/',
                       src: '//player.vimeo.com/video/%id%?autoplay=1'
                     },
-                    gmaps: {
-                      index: '//maps.google.',
-                      src: '%id%&output=embed'
-                    }
                 },
 
                 srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
@@ -302,14 +298,6 @@
                  values.title = item.el.attr('title');
                 }
             },
-        });
-
-        //Google Maps
-        $("#map").googleMap({
-            zoom: 16 // Google Map ZOOM. You can change this value
-        });
-        $("#map").addMarker({
-            address: "S601 Townsend Street, San Francisco, California, USA", // Your Address. Change it
         });
     });
 
